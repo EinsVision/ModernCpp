3375aec97473eb865589583b22c97bd239002759
EinsVision <63795065+EinsVision@users.noreply.github.com>
2020-04-30 08:52:27 +0900

    5.2 조건문 if

--- ModernCpp_OpenCV.cpp
+++ ModernCpp_OpenCV.cpp
@@ -3,7 +3,7 @@
 int main()
 {
     Projects_5 projec;
-    projec.m5_1();
+    projec.m5_2();
 
     return 0;
 }
---
+++ m5_2.cpp
@@ -0,0 +1,21 @@
+#include "projects.h"
+
+void Projects_5::m5_2()
+{
+	// 5.2 Á¶°Ç¹® if
+	int x;
+	cin >> x;
+
+	if (x > 10)
+	{
+		cout << "x is greater than 10" << endl;
+	}
+	else if (x <10)
+	{
+		cout << "x is not greater than 10" << endl;
+	}
+	else
+	{
+		cout << "x is 10" << endl;
+	}
+}
