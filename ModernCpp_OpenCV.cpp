2042904bd020a435d7158598663bd54c7b433094
EinsVision <63795065+EinsVision@users.noreply.github.com>
2020-05-07 14:00:14 +0900

    7.15 명령줄 인수 (command line arguments)

--- ModernCpp_OpenCV.cpp
+++ ModernCpp_OpenCV.cpp
@@ -1,9 +1,24 @@
 ﻿#include "projects.h"
-
-int main()
+          // 개수    // 실제 내용
+int main(int argc, char* argv[])
 {
+    for (int count = 0; count < argc; ++count)
+    {
+        string str = argv[count];
+
+        if (count == 1)
+        {
+            int input_number = stoi(str);     // 문자를 int형으로 변환
+            cout << input_number + 1 << endl; // 문자가 숫자로 변환되었다.
+        }
+        else
+        {
+            cout << str << endl;
+        }
+    }
+    // Property -> Debugging -> Command Arguments 에서 값을 입력!
     Projects_7 projec;
-    projec.m7_14();
+    projec.m7_15();
     
     return 0;
 }
---
+++ m7_15.cpp
@@ -0,0 +1,7 @@
+#include "projects.h"
+
+void Projects_7::m7_15()
+{
+	// 7.15 명령줄 인수 (command line arguments)
+	// main 함수에서 argument를 다룬다.
+}
